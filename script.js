fb926395802db61f7cf689d3edccbc0b11949c4f
Jeevanthok <70204811+Jeevanthok@users.noreply.github.com>
2021-01-08 16:19:41 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {
-    var pos =  document.documentElement.scrollTop;
-    var calc_height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scroll = pos * 100 / calc_height;
-    document.getElementById("progress").style.width= scroll + "%";
-}
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
